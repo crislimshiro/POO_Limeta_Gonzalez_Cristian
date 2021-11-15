45bbc8efccf2f58cb842e0c65c9a33b21ccfa8c1
unknown <c.limeta.g@edu.utc.mx>
2021-11-14 19:17:30 -0600

    Ejemplo de suma de n numeros

---
+++ 02_Entrada_de_Datos/EntradaTexto.java
@@ -0,0 +1,34 @@
+class EntradaTexto{
+    //cuerpo del programa o de la clase
+
+    public static void main(String[] args){
+         //cuerop del metodo principal
+
+         /*
+         vamos a realizar un programa en el cual podamos intruducir
+         texto a nuestra conveniencia
+         */
+        //tipo de dato, nombre de la variable
+        String nombre;
+        //ln en el print es para un salto de linea despues de la impresion en consola
+
+        System.out.println("Por favor introduce tu nombre: ");
+
+        nombre = System.console.realine();
+
+        System.out.println("Hola, "+ nombre+" Bienvenido a tu segundo programa");
+        /*
+        para poder contatenar [agregar a una cadena de texto algo]
+
+        aqui estoy concatenando la variable nombre, para ello se usa + +
+        */
+
+
+
+        
+    }
+}
+    
+
+
+
