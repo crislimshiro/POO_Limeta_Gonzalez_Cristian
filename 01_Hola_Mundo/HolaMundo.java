ac6baa6abef3b8efe9bfa1c53f0eab179bbdc7a6
unknown <c.limeta.g@edu.utc.mx>
2021-11-17 11:50:54 -0600

    Hola Mundo java

--- 01_Hola_Mundo/HolaMundo.java
+++ 01_Hola_Mundo/HolaMundo.java
@@ -1,8 +1,8 @@
 //esto es un comentario
 
 /*
-esto es otro comentario los comentarios no van a afectar 
-al codigo fuente de la clase
+esto es otro comentario los comentarios no van a afectar
+al codigo fuentes de la clase
 */
 
 class HolaMundo{
@@ -17,16 +17,14 @@ variables [globales, independientes o solo de la clase]
 metodos [acciones a programar]
 */
 
-//toda clase debe de tener si solo si es
+//toda clse debe de tener si solo si es 
 //la clase principal un metodo principal
 
-//el metodo principal es donde se hace la llamada 
+//el metodo principal es donde se hace la llamada
 //a las acciones de todo el programa
 
         public static void main(String[] args){
         //esto es el cuerpo del metodo principal
         System.out.print("Hola Mundo... :3");
         }
-
 }
-
