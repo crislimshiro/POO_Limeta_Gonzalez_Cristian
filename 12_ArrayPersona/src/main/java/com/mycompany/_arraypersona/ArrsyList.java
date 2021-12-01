dd1a1ea93923912171eee54a7dac5165229e9324
unknown <c.limeta.g@edu.utc.mx>
2021-12-01 08:12:51 -0600

    ejemplo con netbeans

---
+++ 12_ArrayPersona/src/main/java/com/mycompany/_arraypersona/ArrsyList.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._arraypersona;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ArrsyList<T> {
+
+    public ArrsyList() {
+    }
+    
+}
